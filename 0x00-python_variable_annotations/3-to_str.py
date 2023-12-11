bc8040a0350b8527a91efe7ceb2c80708feb48c6
Paul John <rainson.work@gmail.com>
2023-12-11 18:19:27 +0300

    A type-annotated function to cast a float to a string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""A type-annotated function to cast an float to a string"""
+
+
+def to_str(n: float) -> str:
+    """Cast a float to a string
+
+    Args:
+        n (float): A float to convert to a string
+
+    Returns:
+        str: A string representation of a float
+
+    """
+    return str(n)
