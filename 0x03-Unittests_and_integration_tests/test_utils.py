ef16aa9f56becf69199adc35a4af9d450394380d
Paul John <rainson.work@gmail.com>
2024-01-07 17:57:32 +0300

    Add test method for memoize wrapper method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -67,3 +67,29 @@ class TestGetJson(unittest.TestCase):
         with patch("requests.get", return_value=Mock(**attrs)) as get_request:
             self.assertEqual(get_json(test_url), test_payload)
             get_request.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Tests the memoize decorator method"""
+
+    def test_memoize(self) -> None:
+        """Test memoize() wrapper method"""
+
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(
+            TestClass,
+            "a_method",
+            return_value=lambda: 42,
+        ) as memo_method:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+
+            memo_method.assert_called_once()
